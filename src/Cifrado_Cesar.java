658b6fddc9fdd7cda49f768c3e0362a6acb5b6fd
Leam Fernando Ruiz Panchana <leam.ruizp@ug.edu.ec>
2024-09-18 00:38:57 -0500

    feature: Avance de Cipher

--- src/Cifrado_Cesar.java
+++ src/Cifrado_Cesar.java
@@ -1,13 +1,27 @@
 public class Cifrado_Cesar {
-    static char[] alfabeto = new char [] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+    private static final char[] alfabeto = new char [] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
             'ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
     static int clave = 1;
-    static String mensaje = "abcd";
+    static String mensaje = "zbgh";
     static String mensajecifrado;
 
     public Cifrado_Cesar(int clave, String mensaje) {
         this.clave = clave;
         this.mensaje = mensaje;
+    }
+
+    public static String encrypt() {
+        StringBuffer cifrado = new StringBuffer();
+        for (char letras : mensaje.toCharArray()) {
+            int posicion = new String(alfabeto).indexOf(letras);
+            if (posicion != 0) {
+                int nuevaPosicion = (posicion + clave) % alfabeto.length;
+                cifrado.append(alfabeto[nuevaPosicion]);
+            } else {
+                cifrado.append(letras); // No cambia caracteres que no están en el alfabeto
+            }
+        }
+            return cifrado.toString();
     }
 
 }
